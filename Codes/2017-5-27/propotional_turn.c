ba4b6c5440b9152151ab86c927864527edf6e00c
Hari <coolhari1996@gmail.com>
2017-07-05 10:45:51 +0530

    PID code commented

--- Codes/2017-5-27/propotional_turn.c
+++ Codes/2017-5-27/propotional_turn.c
@@ -1,17 +1,26 @@
+/*
+Author : R Hariharan
+Description: Example code to potray the behaviour of PID controller for angular velocity
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
+//Define the gains here
 #define KP 20
 #define KI 10
 #define KD 0
 
+//Function that calculates the angular velocity based of the error in the current angle
 int propotional_rotate(int error_angle)
 {
-	int total_error = 0;
+	int total_error = 0; 
 	int previous_error = error_angle;
 	float angular_velocity = 0;
+	// Controller Loop
 	while(error_angle != 0)
 	{
+		// New angular_velocity = Propotional_gain * error_in_angle + Integration_gain * (total_error_till_now) + Differential_gain * (change_in_error_from_the_previous_error)
 		angular_velocity = KP * error_angle + KI * (total_error + error_angle) + KD * (previous_error - error_angle);
 		previous_error = error_angle;
 		total_error += error_angle;
@@ -21,7 +30,8 @@ int propotional_rotate(int error_angle)
 		error_angle -= angular_velocity  * 0.02;
 	}
 }
+
 int main()
 {
-	propotional_rotate(225);
+	propotional_rotate(225); //Rotate to 225
 }
