e3b82543a7d47017001e77b3859bb74fd1c8af5b
Hari <coolhari1996@gmail.com>
2017-07-05 11:41:46 +0530

    Rendezous problem C program commented

--- Codes/2017-5-30/Randevous_Problem.c
+++ Codes/2017-5-30/Randevous_Problem.c
@@ -1,3 +1,10 @@
+/*
+Author : R Hariharan
+Description: This is a C Code to simuate the working of the Rendezous Prblem. We consider here
+that the robot can sense the surrounding using IR proximity sensor (8 placed 45 degrees apart).
+The centroid of the shape formed by all the robots as vertices is calculated. This is the point 
+the robot travels to. 
+*/
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -6,6 +13,7 @@
 
 #define PI 3.14159265
 
+//Function to generate random sensor Values
 int get_sensor_value()
 {
 	time_t t;
@@ -13,15 +21,21 @@ int get_sensor_value()
 	return rand() % 150;		
 }
 
+//Function which is used to get all 8 sensor values
 void generate_sensor_values(int * array)
 {
-	// int i;
-	// for(i=0;i<8;i++)
-	// {
-	// 	// array[i] = get_sensor_value();
-	// 	// sleep(1);
-	// 	scanf("%d",&array[i]);
-	// }
+	//The below code is used to generate random sensor values
+	/*
+	int i;
+	for(i=0;i<8;i++)
+	{
+		// array[i] = get_sensor_value();
+		// sleep(1);
+		scanf("%d",&array[i]);
+	}
+	*/
+
+	//Below code is to enter the sensor values manually 
 	array[0] = 150;
 	array[1] = 150;
 	array[2] = 40;
@@ -30,9 +44,11 @@ void generate_sensor_values(int * array)
 	array[5] = 150;
 	array[6] = 150;
 	array[7] = 150;
+
 	return;
 }
 
+//Function to print the integer values of the array of size n
 void print_sensor_values(char * string,int * array,int n)
 {
 	int i;
@@ -45,31 +61,38 @@ void print_sensor_values(char * string,int * array,int n)
 	return;
 }
 
+//Function to find the centroid of the shape formed by the robots
 int find_centroid(int * sensor_values,int * centroid)
 {
-	float x = 0;
-	float y = 0;
+	float x = 0; //x co-ordinate of centroid
+	float y = 0; //y co-ordinate of centroid
 	int i;
-	int counter = 0;
+	int counter = 0; //Count of robots in the shape (Number of vertices)
+	//8 sensors - run 8 times
 	for(i=0;i<8;i++)
 	{
+		//Check if the robots is in range (100 mm)
 		if(sensor_values[i] < 100)
 		{
 			printf("%d %f %f\n",sensor_values[i], sensor_values[i] * cos(i * 45 * PI / 180), sensor_values[i] * sin(i * 45 * PI / 180));
 			printf("Angle : %f\n", i * 45 * PI / 180);
+			//change the polar co-ordinates to cartesian co-ordinates
 			x += sensor_values[i] * cos(i * 45 * PI / 180);
 			y += sensor_values[i] * sin(i * 45 * PI / 180);
 			counter++;
 		}
 	}
 	printf("Counter :: %d\n", counter);
+	//Centroid = (sum of all co-ordinates)/(number of vertices)
 	x = x/(counter+1);
-	y = y/(counter+1);
+	y = y/(counter+1); 
 	printf("x: %f y: %f\n", x,y);
-	int distance = sqrt(pow(x,2)+pow(y,2));
+
+	int distance = sqrt(pow(x,2)+pow(y,2)); //Distance to move by (relative co-odinate from (0,0) where the robot lies)
 	printf("%f\n", y/x);
-	int angle = atan(y/x) * 180 / PI;
+	int angle = atan(y/x) * 180 / PI; //Angle to rotate by (Robot allways faces in the positive x direction)
 	printf("distance: %d angle: %d\n", distance,angle);
+	//atan gives value between (90,-90), do need to change it between (0,360)
 	if(x < 0 && y > 0)
 	{
 		angle = 90 - angle;
@@ -84,6 +107,7 @@ int find_centroid(int * sensor_values,int * centroid)
 	return 0;
 }
 
+//Call different functions, and analyse the output
 int main()
 {
 	int bot1_sensor_values[8];
@@ -97,6 +121,6 @@ int main()
 	find_centroid(bot1_sensor_values,bot1_centroid);
 	// find_centroid(bot2_sensor_values,bot2_centroid);
 	print_sensor_values("Centroid Bot 1",bot1_centroid,2);
-	// print_sensor_values("Centroid Bot 2",bot2_centroid,2);
+	// print_sensor_values("Centroid Bot 2",bot2_csentroid,2);
 	return 0;
 }
